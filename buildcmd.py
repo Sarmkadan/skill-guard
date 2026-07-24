54f42fdc6ecd7f750b3c08e397e277e52500895a
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 11:34:49 +0300

    chore: buildcmd changes

--- buildcmd.py
+++ buildcmd.py
@@ -8,3 +8,19 @@ print("Running tests...")
 # For example, you can add a command to build the project
 print("Building project...")
 # Add your build code here
+
+# Run the tests
+print("Running tests...")
+# Add your test running code here
+# For example:
+# import unittest
+# import sys
+# sys.path.insert(0, '/home/redrocket/task-factory/workdir/skill-guard')
+# unittest.main(module='tests.SkillGuard.Tests')
+
+# Run the build
+print("Building project...")
+# Add your build code here
+# For example:
+# import subprocess
+# subprocess.run(['dotnet', 'build'])
