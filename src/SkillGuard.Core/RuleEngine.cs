8662194150d9e92de5a7ef680b83889e4080fe02
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-31 07:23:12 +0000

    chore: apply codex edits

--- src/SkillGuard.Core/RuleEngine.cs
+++ src/SkillGuard.Core/RuleEngine.cs
@@ -4,45 +4,9 @@ using System.Diagnostics;
 
 namespace SkillGuard.Core;
 
-public sealed class RuleEngine(IReadOnlyList<IScanRule> rules) : IScanner, IRuleEngine, IEquatable<RuleEngine>
+public sealed class RuleEngine(IReadOnlyList<IScanRule> rules) : IScanner, IRuleEngine
 {
     public IReadOnlyList<IScanRule> Rules { get; } = rules ?? throw new ArgumentNullException(nameof(rules));
-
-    public bool Equals(RuleEngine? other)
-    {
-        if (other is null) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Rules.SequenceEqual(other.Rules);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (obj is null) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != GetType()) return false;
-        return Equals((RuleEngine)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        var hash = new HashCode();
-        foreach (var rule in Rules)
-        {
-            hash.Add(rule);
-        }
-        return hash.ToHashCode();
-    }
-
-    public static bool operator ==(RuleEngine? left, RuleEngine? right)
-    {
-        if (left is null) return right is null;
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(RuleEngine? left, RuleEngine? right)
-    {
-        return !(left == right);
-    }
 
     public ScanReport Scan(IEnumerable<ScanTarget> targets)
     {
